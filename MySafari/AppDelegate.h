bbdc9900d0919bcae965d3190fba5fb467eb0ce5
Poulose Matthen <poulosem@gmail.com>
2014-01-09 13:48:02 -0600

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Apple on 09/01/14.
+//  Copyright (c) 2014 Tablified Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Apple on 09/01/14.
+//  Copyright (c) 2014 Tablified Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
