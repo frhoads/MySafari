9e52ec72a672e66b93bef3384760f3844a20a733
Poulose Matthen <poulosem@gmail.com>
2014-01-09 15:39:56 -0600

    MVP and Stretch Goals complete

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
+{
+    
+    __weak IBOutlet UIWebView *myWebView;
+    __weak IBOutlet UITextField *myURLTextField;
+}
 
 @end
 
@@ -18,7 +23,67 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    myWebView.scrollView.delegate = self;
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSURL *url;
+    
+    if ([myURLTextField.text hasPrefix:@"http://"]) {
+        url = [NSURL URLWithString:myURLTextField.text];
+    } else {
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", myURLTextField.text]];
+    }
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [myWebView loadRequest:request];
+    [myURLTextField resignFirstResponder];
+
+    return YES;
+}
+
+- (IBAction)onBackButtonPressed:(id)sender {
+    [myWebView goBack];
+}
+
+- (IBAction)onForwardButtonPressed:(id)sender {
+    [myWebView goForward];
+}
+
+- (IBAction)onStopLoadingButtonPressed:(id)sender {
+    [myWebView stopLoading];
+}
+
+- (IBAction)onReloadButtonPressed:(id)sender {
+    [myWebView reload];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGPoint translation = [scrollView.panGestureRecognizer translationInView:scrollView.superview];
+    
+    if(translation.y > 0)
+    {
+        NSLog(@"Scrolling Down");
+        myURLTextField.alpha += 0.05;
+        myURLTextField.frame = CGRectMake(myURLTextField.frame.origin.x, 30, myURLTextField.frame.size.width, myURLTextField.frame.size.height);
+        
+    } else
+    {
+        NSLog(@"Scrolling Up");
+        myURLTextField.alpha -= 0.05;
+        myURLTextField.frame = CGRectMake(myURLTextField.frame.origin.x, (myURLTextField.frame.origin.y + 10), myURLTextField.frame.size.width, myURLTextField.frame.size.height);
+
+    }
+}
+
+- (IBAction)onTeaserButtonPressed:(id)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Coming Soon" message:nil delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    
+    [alert show];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
